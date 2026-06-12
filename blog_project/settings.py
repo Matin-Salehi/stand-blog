610cab9b3127cfbbe9945909f538fad6f9554252
matin salehi <symatin3@gmail.com>
2026-06-12 16:25:26 +0330

    add login form

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'home_app.apps.HomeAppConfig',
+    'users_app.apps.UsersAppConfig',
 ]
 
 MIDDLEWARE = [
