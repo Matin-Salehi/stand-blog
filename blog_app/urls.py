cc8d186afc6e24b8f56aac849e51ad0a1224845e
matin salehi <symatin3@gmail.com>
2026-06-22 18:30:50 +0330

    feat: add blog entries to view all post

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'blog_app'
 urlpatterns = [
     path('detail/<slug:slug>', views.post_detail, name='detail'),
+    path('list', views.post_list, name='post_list'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -6,3 +6,7 @@ from blog_app.models import Post
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'blog_app/post-details.html', {'post': post})
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog_app/blog.html', {'posts': posts})
